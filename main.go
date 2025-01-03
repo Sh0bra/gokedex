79f4acbda1fbbd09c9597d83d4fe65e9e3f0c0f7
Sh0bra <jshin1@cpp.edu>
2025-01-02 19:49:19 -0800

    fixed unit repl_test.go

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter Pokemon name: ")
+		text, _ := reader.ReadString('\n')
+		cleaned := cleanInput(text)
+		if text == "exit\n" {
+			fmt.Println("Thank you for using gokedex!")
+			break
+		}
+		fmt.Println(cleaned)
+	}
 }
 
-func cleanInput(text string) []string {
-	s := []string()
-	return s
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
 }
--- repl_test.go
+++ repl_test.go
@@ -1,11 +1,47 @@
-package gokedex
+package main
 
 import (
 	"testing"
 )
 
 func TestCleanInput(t *testing.T) {
-	fmt.Println("testing")
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{
+				"hello",
+				"world"},
+		},
+		// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("Length does not match: %v vs %v",
+				len(actual),
+				len(c.expected),
+			)
+			continue
+		}
+		for i := range actual {
+			actualWord := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			if actualWord != expectedWord {
+				t.Errorf("%v does not equal %v",
+					actualWord,
+					expectedWord,
+				)
+			}
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+		}
+	}
 }
-
-
